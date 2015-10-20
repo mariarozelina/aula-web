0583fe1c8324cb06f3b06f87b28dc78c198636d7
Maria Rozelina <mariarozelina@gmail.com>
2015-10-20 20:15:01 -0200

    primeiro

---
+++ arquivo.py
@@ -0,0 +1,9 @@
+arquivo = open('numeros.txt','w')
+
+for linha in range(1,101):
+    arquivo.write('%d\n'%linha)
+
+
+
+
+arquivo.close()
---
+++ leitura de arquivos.py
@@ -0,0 +1,6 @@
+arquivo = open('numeros.txt','r')
+
+for linha in arquivo.readlines():
+    print(linha)
+
+arquivo.close()
